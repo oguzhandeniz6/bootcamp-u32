4ca57d238ef02c4e2dd661bcdb9be1c5ee1af77e
Arda <mortusdorentum@gmail.com>
2023-06-11 20:43:00 +0300

    Grabbable Scripts
    -Grabbable
    -Consumable
    -PlayerController

---
+++ Assets/Scripts/Grabbables/PlayerController.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform rightHand;
+
+    [Tooltip("Fýrlatma ve yere býrakmayý ayýran delay süresi")]
+    [SerializeField] private float delayForThrowing = 0.3f;
+    private float holdStartTime = 0;
+
+
+    private void Update()
+    {
+        //Throw tuþu için ---------------------------------------------------
+        //Elinde item varken
+        if (rightHand.childCount > 0)
+        {
+            //Space'e basarsa
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Space'e basma zamanýndan itibaren geçen zamaný tut
+                holdStartTime = Time.time;
+                //Fýrlatma animasyonuna baþla
+
+            }
+            //Space'den çekerse
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+
+                //Geçen zamanýn max zamana uygun olduðunu checkle
+                float passedTime = Time.time - holdStartTime;
+
+                //Throwa geçmeden space býrakýldýysa
+                if (passedTime < delayForThrowing)
+                {
+                    //Yere býrak
+                    Drop();
+
+                }
+                //Throw süresini aþtýysa
+                if (passedTime > delayForThrowing)
+                {
+                    //Fýrlat
+                    Throw();
+                }
+
+            }
+
+        }
+        //Use tuþu için ----------------------------------------------------
+        //Elinde obje varsa
+        if (rightHand.childCount > 0)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                rightHand.GetChild(0).GetComponent<Grabbable>().Use();
+
+
+            }
+        }
+        //Elinde obje yoksa
+        if (objectsInRadius.Count > 0 && rightHand.childCount == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PickUp(FindNearestItem());
+            }
+        }
+
+
+
+    }
+
+
+    [SerializeField] private List<GameObject> objectsInRadius = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.TryGetComponent<Grabbable>(out Grabbable triggerObject))
+        {
+
+            objectsInRadius.Add(other.gameObject);
+
+
+        }
+
+    }
+
+
+
+    private GameObject FindNearestItem()
+    {
+
+        GameObject nearestItem = null;
+        float nearestDistance = Mathf.Infinity;
+
+        foreach (GameObject go in objectsInRadius)
+        {
+            float distance = Vector3.Distance(transform.position, go.transform.position);
+
+            if (distance < nearestDistance)
+            {
+                nearestDistance = distance;
+                nearestItem = go;
+            }
+
+        }
+
+        return nearestItem;
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (objectsInRadius.Contains(other.gameObject))
+        {
+
+            objectsInRadius.Remove(other.gameObject);
+        }
+    }
+
+
+    private void PickUp(GameObject pickedObject)
+    {
+        pickedObject.transform.SetParent(rightHand);
+        pickedObject.transform.localPosition = Vector3.zero;
+        pickedObject.GetComponent<Rigidbody>().isKinematic = true;
+        pickedObject.GetComponent<MeshCollider>().enabled = false;
+    }
+
+    private void Drop()
+    {
+        var child = rightHand.GetChild(0);
+
+        child.transform.SetParent(null);
+        child.GetComponent<Rigidbody>().isKinematic = false;
+        child.GetComponent<MeshCollider>().enabled = true;
+
+
+    }
+
+    //Þimdilik drop ile ayný
+    private void Throw()
+    {
+        var child = rightHand.GetChild(0);
+
+        child.transform.SetParent(null);
+        child.GetComponent<Rigidbody>().isKinematic = false;
+        child.GetComponent<MeshCollider>().enabled = true;
+
+
+    }
+
+}
+
