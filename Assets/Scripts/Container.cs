f88ad457fc76bff67a1ff852c1fa1b8c7c6287a6
Arda <mortusdorentum@gmail.com>
2023-06-16 23:22:03 +0300

    Kutu hasar alma animasyonu
    Dotween ile yapıldı

--- Assets/Scripts/Container.cs
+++ Assets/Scripts/Container.cs
@@ -17,7 +17,8 @@ public class Container : MonoBehaviour, IDamagable
     {
         Health -= 1;
         Debug.Log(Health);
-        gameObject.transform.DOShakePosition(0.3f, 0.1f, 25);
+        //Vurulduðu zaman titreme ve kýzarma efekti
+        gameObject.transform.DOShakePosition(0.3f, new Vector3(0.1f, 0, 0.1f), 25);
         gameObject.GetComponent<MeshRenderer>().material.DOColor(Color.red, 0.3f).From();
         if(Health <= 0)
         {
