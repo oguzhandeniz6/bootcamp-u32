dc4449eab20e2a7a9c102ba7f252390f32fe9817
Arda <mortusdorentum@gmail.com>
2023-07-11 00:03:47 +0300

    .

---
+++ Assets/Scripts/AI Scripts/AIAttackState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAttackState : AIState
+{
+    public void Enter(AIAgent agent)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Exit(AIAgent agent)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public AiStateId GetId()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Update(AIAgent agent)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
